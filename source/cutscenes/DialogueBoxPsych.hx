88023a4bbcc65ed7bead671a3c401fb0729f39b5
CharlesCatYT <62410083+CharlesCatYT@users.noreply.github.com>
2023-09-13 21:28:05 -0400

    Update DialogueBoxPsych.hx

--- source/cutscenes/DialogueBoxPsych.hx
+++ source/cutscenes/DialogueBoxPsych.hx
@@ -12,8 +12,6 @@ import objects.TypedAlphabet;
 
 import cutscenes.DialogueCharacter;
 
-// Gonna try to kind of make it compatible to Forever Engine,
-// love u Shubs no homo :flushedh4:
 typedef DialogueFile = {
 	var dialogue:Array<DialogueLine>;
 }
